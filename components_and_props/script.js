96ba53f7388d9380d85f2da730deec15f93e94ec
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-09-05 15:46:22 +0100

    extracted a UserInfo component

--- components_and_props/script.js
+++ components_and_props/script.js
@@ -13,20 +13,28 @@ const Avatar = function(props) {
     );
 };
 
+
+const UserInfo = function(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                 {props.user.name}
+            </div>
+        </div>
+    );
+};
+
+
 const Comment = function(props) {
     return (
         <div className="Comment">
-            <div className="UserInfo">
-                <Avatar user={props.author} />
-                <div className="UserInfo-name">
-                     {props.author.name}
-                </div>
-                <div className="Comment-text">
-                    {props.text}
-                </div>
-                <div className="Comment-date">
-                    {formatDate(props.date)}
-                </div>
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
             </div>
         </div>
     );
