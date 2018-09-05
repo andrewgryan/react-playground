2c0aeffbbea5ff47ffce2684ec38817987fa2e77
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-09-05 14:05:38 +0100

    generalised to use a function to format a name

--- hello_world/hello.js
+++ hello_world/hello.js
@@ -1,4 +1,10 @@
 'use strict';
-const name = "Bob";
-const element = <h1>Hello, { name }!</h1>;
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+};
+const user = {
+    firstName: "Bob",
+    lastName: "Martin"
+};
+const element = <h1>Hello, { formatName(user) }!</h1>;
 ReactDOM.render(element, document.querySelector('#hello'));
