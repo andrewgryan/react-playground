e9babf644348bca75fc4e2cb0d569f496a52e2c7
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-09-05 14:52:06 +0100

    demonstrate JSX used as a JS object

--- hello_world/hello.js
+++ hello_world/hello.js
@@ -2,9 +2,15 @@
 function formatName(user) {
     return user.firstName + ' ' + user.lastName;
 };
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, stranger!</h1>;
+};
 const user = {
     firstName: "Bob",
     lastName: "Martin"
 };
-const element = <h1>Hello, { formatName(user) }!</h1>;
+const element = getGreeting(user);
 ReactDOM.render(element, document.querySelector('#hello'));
