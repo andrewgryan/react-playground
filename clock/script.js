73e02929f39a9cc4bc980096fbb55eaac54c80b1
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-09-05 16:26:03 +0100

    made an App component with 3 clocks

--- clock/script.js
+++ clock/script.js
@@ -16,13 +16,21 @@ class Clock extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello, world!</h1>
                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
     }
 }
+const App = function() {
+    return (
+        <div>
+            <Clock />
+            <Clock />
+            <Clock />
+        </div>
+    );
+};
 ReactDOM.render(
-    <Clock />,
+    <App />,
     document.getElementById('root')
 );
