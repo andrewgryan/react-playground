f25f488c1de5962e3b5d5562296d28e322a71263
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-09-05 15:14:55 +0100

    first foray into react components

---
+++ components_and_props/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}!</h1>;
+}
+
+const element = <Welcome name="Sarah" />;
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
