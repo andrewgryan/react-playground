4b4a27054babed604bd2ed5975a23ac4fb7ddd8f
andrew.ryan <andrew.ryan@metoffice.gov.uk>
2018-09-05 15:34:42 +0100

    Added a complex component that needs refactoring

--- components_and_props/script.js
+++ components_and_props/script.js
@@ -1,16 +1,38 @@
 'use strict';
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}!</h1>;
-}
+const formatDate = function(text) {
+    return text;
+};
 
+const Comment = function(props) {
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar"
+                     src={props.author.avatarUrl}
+                     alt={props.author.name}
+                />
+                <div className="UserInfo-name">
+                     {props.author.name}
+                </div>
+                <div className="Comment-text">
+                    {props.text}
+                </div>
+                <div className="Comment-date">
+                    {formatDate(props.date)}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const author = { name: 'Andy', avatarUrl: 'image.png'};
 function App() {
     return (
         <div>
-            <Welcome name="Alice" />
-            <Welcome name="Bob" />
-            <Welcome name="Sarah" />
-            <Welcome name="Steve" />
+            <Comment author={author}
+                     text="Lorem ipsum"
+                     date="2018-01-01 00:00:00" />
         </div>
     );
 }
