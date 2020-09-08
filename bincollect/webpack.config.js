be4b3c05523b4c72bccb92484368b714056807c8
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-09-08 16:05:56 +0100

    Hello, World React app

---
+++ bincollect/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
+    },
+    plugins: [ new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }) ]
+}
